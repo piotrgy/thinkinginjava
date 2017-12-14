feac39eaa3e0f3e0af6b8de502e0512726a35797
piotrgy <piotr_gy@wp.pl>
2017-12-14 14:13:16 +0100

    Forced garbage collector.

--- src/main/java/piotrgorny/App.java
+++ src/main/java/piotrgorny/App.java
@@ -1,13 +1,12 @@
 package piotrgorny;
 
-/**
- * Hello world!
- *
- */
+import piotrgorny.chapter5.Ex10;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        // chapter 5 ex. 10
+        new Ex10("Drive");
+        System.gc();
     }
 }
