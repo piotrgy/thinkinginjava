3158d64ecc3d117bb54c55880a8c2f09228fca62
piotrgy <piotr_gy@wp.pl>
2017-12-14 14:36:23 +0100

    Checking the end condition.

--- src/main/java/piotrgorny/App.java
+++ src/main/java/piotrgorny/App.java
@@ -1,12 +1,23 @@
 package piotrgorny;
 
 import piotrgorny.chapter5.Ex10;
+import piotrgorny.chapter5.Tank;
 
 public class App 
 {
     public static void main( String[] args ){
         // chapter 5 ex. 10
-        new Ex10("Drive");
+        /*new Ex10("Drive");
+        System.gc();*/
+
+        // chapter 5 ex. 12
+        new Tank();
+        System.gc();
+
+        new Tank().fill();
+        System.gc();
+
+        new Tank().empty();
         System.gc();
     }
 }
